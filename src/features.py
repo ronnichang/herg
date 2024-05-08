7cd2f081655f7ef9606c1b9ff6fee3ce5c12c0d5
Ronni Chang <changsh8@uscacl-0-34.nibr.novartis.net>
2024-05-08 10:41:02 -0400

    Make number of features from SMILES be 128 across all experiments

--- src/features.py
+++ src/features.py
@@ -3,7 +3,7 @@ from rdkit.Chem import AllChem
 
 
 # Function to convert SMILES to Morgan fingerprints
-def smiles_to_fp(smiles, radius=2, nBits=1024):
+def smiles_to_fp(smiles, radius=2, num_bits=1024):
     mol = Chem.MolFromSmiles(smiles)
-    return list(AllChem.GetMorganFingerprintAsBitVect(mol, radius, nBits))
+    return list(AllChem.GetMorganFingerprintAsBitVect(mol, radius, num_bits))
 
